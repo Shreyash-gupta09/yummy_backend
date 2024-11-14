96ad77ae96d723ec1ece6542447de26a05261026
Shreyash-gupta09 <ShreyashKumar.Gupta@iiitb.ac.in>
2024-11-14 11:15:13 +0530

    Initialized Project

---
+++ src/main/java/com/shreyash/yummy_new/entity/Customer.java
@@ -0,0 +1,4 @@
+package com.shreyash.yummy_new.entity;
+
+public class Customer {
+}
---
+++ src/test/java/com/shreyash/yummy_new/YummyNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shreyash.yummy_new;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YummyNewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
