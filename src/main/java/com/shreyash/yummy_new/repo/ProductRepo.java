60a4da73c0d2a3b1fbda069aa02edc65e37fb019
Shreyash-gupta09 <ShreyashKumar.Gupta@iiitb.ac.in>
2024-11-20 01:21:14 +0530

    Added Products Table and done CRUD opertaions

---
+++ src/main/java/com/shreyash/yummy_new/repo/ProductRepo.java
@@ -0,0 +1,12 @@
+package com.shreyash.yummy_new.repo;
+
+
+import com.shreyash.yummy_new.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import java.util.List;
+public interface ProductRepo extends JpaRepository<Product, Long> {
+    @Query("SELECT p FROM Product p WHERE p.price BETWEEN :minPrice AND :maxPrice ORDER BY p.price ASC")
+    List<Product> findTop2ByPriceBetweenOrderByPriceAsc(@Param("minPrice") double minPrice, @Param("maxPrice") double maxPrice);
+}
---
+++ src/main/java/com/shreyash/yummy_new/service/ProductService.java
@@ -0,0 +1,28 @@
+package com.shreyash.yummy_new.service;
+
+import com.shreyash.yummy_new.entity.Product;
+import com.shreyash.yummy_new.repo.ProductRepo;
+import org.springframework.stereotype.Service;
+import java.util.List;
+@Service
+public class ProductService {
+    private final ProductRepo productRepository;
+    public ProductService(ProductRepo productRepository) {
+        this.productRepository = productRepository;
+    }
+    public Product saveProduct(Product product) {
+        return productRepository.save(product);
+    }
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+    public Product getProductById(Long id) {
+        return productRepository.findById(id).orElse(null);
+    }
+    public void deleteProduct(Long id) {
+        productRepository.deleteById(id);
+    }
+    public List<Product> getProductsByPriceRange(double minPrice, double maxPrice) {
+        return productRepository.findTop2ByPriceBetweenOrderByPriceAsc(minPrice, maxPrice);
+    }
+}
